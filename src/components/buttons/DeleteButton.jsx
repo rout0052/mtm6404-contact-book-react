ebc49aa293a67b855a729e7f95ba2c28a722a950
Taro Routly <rout0052@algonquinlive.com>
2026-08-16 17:35:31 -0400

    Adds functionality to delete button with an optional refresh if deleted from the homepage

--- src/components/buttons/DeleteButton.jsx
+++ src/components/buttons/DeleteButton.jsx
@@ -1,8 +1,38 @@
-const DeleteButton = ({id, isCircle, children}) => {
+import { useNavigate } from 'react-router-dom';
+import { doc, deleteDoc } from 'firebase/firestore';
+
+import db from '../../utils/db.js';
+
+const DeleteButton = ({ id, isCircle, refreshContacts, children }) => {
+    const navigate = useNavigate();
+
+    const deleteContact = async (id) => {
+        const docRef = doc(db, "contacts", id);
+
+        try {
+            // Uses a confirm window to check if the user is sure
+            if (window.confirm("Are you sure you want to delete this contact? This action cannot be undone")) {
+                // Set the value of contacts to all of the contacts, minus the id 
+                console.log("Deletes the contact:" + id)
+                await deleteDoc(docRef);
+
+                if(refreshContacts) {
+                    refreshContacts();
+                    document.getElementById('sortByLastName').click();
+                }
+
+                navigate('/');
+            }
+        } catch (err) {
+            // Sends an alert that the deletion failed
+            alert('The user was unable to be deleted, please try again');
+            console.error(err);
+        }
+    }
 
     return (
-        <button className={`btn btn-soft btn-error ${ isCircle ? 'btn-circle' : 'rounded-full'}`} aria-label="Delete contact">
-        { children ? children : ''}<i className="fa-solid fa-trash"></i>
+        <button className={`btn btn-soft btn-error ${isCircle ? 'btn-circle' : 'rounded-full'}`} aria-label="Delete contact" onClick={() => deleteContact(id)}>
+            {children ? children : ''}<i className="fa-solid fa-trash"></i>
         </button>
     )
 }
