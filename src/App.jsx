e8fe8f037e6c5f2156bfe927b91422762480e30d
Taro Routly <rout0052@algonquinlive.com>
2026-08-16 21:47:56 -0400

    Adds comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+// Imports hooks and components from node modules
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 
+// Imports components
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 
 import './App.css'
 
 function App() {
-
   return (
     <>
       <Header />
+      {/* Wraps the Outlet that switches the routes in a main tag, and styles it */}
       <main className="mx-auto max-w-200 my-8">
         <Outlet />
       </main>
